062a16e4ea58f61d34a47f9e6e5bc60648b14c82
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-09-01 12:20:52 +0200

    Add __all__ to __init__

--- e2xgradingtools/__init__.py
+++ e2xgradingtools/__init__.py
@@ -2,3 +2,5 @@ from .classes import ClassTest
 from .functions import FunctionTest
 from .utils import grade_report
 from .variables import VariableTest
+
+__all__ = ["ClassTest", "FunctionTest", "grade_report", "VariableTest"]
